1e70c5bf9e21796a0be8235f1195bf370870589f
sasaki takumu <takumu.sasaki.sys20@morijyobi.ac.jp>
2021-03-26 11:05:49 +0900

    BMIを2回表示する仕様変更

--- main.py
+++ main.py
@@ -4,4 +4,5 @@ print("BMI計算アプリ")
 w = int(input("体重(kg):"))
 h = int(input("身長(cm):"))
 h = h / 100
-print(f"{calc_bmi.bmi_calc(h, w)}")
+for i in range(2):
+    print(f"BMI:{calc_bmi.bmi_calc(h, w)}")
